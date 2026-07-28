f8aa90b08f67f76a2f2296b5e802e6d4c9180813
Omar Bouslime <bouslimeomar@gmail.com>
2026-07-28 16:29:46 +0100

    feat-add-comment-story-user-topic-publication-entities

---
+++ src/main/java/com/Omarb17/medium_clone/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.Omarb17.medium_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "story_id")
+    private Story story;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
