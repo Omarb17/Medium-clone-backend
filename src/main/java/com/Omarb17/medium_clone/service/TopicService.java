2f0f3e05b14339fbdea52a5f99214f60998bea0d
Omar Bouslime <bouslimeomar@gmail.com>
2026-07-29 20:32:36 +0100

    feat-add-topic-service

---
+++ src/main/java/com/Omarb17/medium_clone/service/TopicService.java
@@ -0,0 +1,32 @@
+package com.Omarb17.medium_clone.service;
+
+import com.Omarb17.medium_clone.entity.Topic;
+import com.Omarb17.medium_clone.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TopicService {
+
+    private final TopicRepository topicRepository;
+
+    @Autowired
+    public TopicService(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public Optional<Topic> getTopicById(Long id) {
+        return topicRepository.findById(id);
+    }
+
+    public Topic addNewTopic(Topic topic) {
+        return topicRepository.save(topic);
+    }
+}
