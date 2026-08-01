7b63eb82d6235ba08e167d359676d72de3cd7e72
Omar Bouslime <bouslimeomar@gmail.com>
2026-08-01 19:09:57 +0100

    feat-add-story-controller

---
+++ src/main/java/com/Omarb17/medium_clone/controller/StoryController.java
@@ -0,0 +1,57 @@
+package com.Omarb17.medium_clone.controller;
+import com.Omarb17.medium_clone.entity.Story;
+
+import com.Omarb17.medium_clone.service.StoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/stories")
+public class StoryController {
+
+    private final StoryService storyService;
+
+    @Autowired
+    public StoryController(StoryService storyService) {
+        this.storyService = storyService;
+    }
+
+    @Operation(summary = "Get all stories")
+    @GetMapping
+    public List<Story> getStoriesList() { return storyService.getAllStories();}
+
+    @Operation(summary = "Get story by id")
+    @GetMapping("/{id}")
+    public ResponseEntity<Story> getStoryById (@PathVariable Long id) { return storyService.getStoryById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());}
+
+    @Operation(summary = "Add a new story")
+    @PostMapping
+    public ResponseEntity<Story> addNewStory(@RequestBody Story story) {
+        Story savedStory = storyService.addNewStory(story);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedStory);
+    }
+
+    @Operation(summary = "Edit Story")
+    @PutMapping("/{id}")
+    public ResponseEntity<Story> editStory (@PathVariable Long id, @RequestBody Story story)
+    {return storyService.updateStory(id, story.getTitle(), story.getText())
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());}
+
+    @Operation(summary = "Delete Story")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStory (@PathVariable Long id) {
+        return storyService.deleteStory(id) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.notFound().build();}
+
+
+}
