d522c2c21a945f1cc75dadc3535ed2da5e05a706
Omar Bouslime <bouslimeomar@gmail.com>
2026-08-07 23:45:40 +0100

    feat-add-validations-for-all-entities

--- src/main/java/com/Omarb17/medium_clone/entity/Comment.java
+++ src/main/java/com/Omarb17/medium_clone/entity/Comment.java
@@ -1,6 +1,8 @@
 package com.Omarb17.medium_clone.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,8 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "content is required")
+    @Size(min = 2, max = 50, message = "content must be between 2 and 50 characters")
     private String content;
 
     @ManyToOne
