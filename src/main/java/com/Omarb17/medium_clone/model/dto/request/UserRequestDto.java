e92b1cba65a9c88fb124a14d9add1f4a063c5e8c
Omar Bouslime <bouslimeomar@gmail.com>
2026-08-25 18:12:19 +0100

    feat-add-handleValidationErrors-to-GlobalExceptionHandler

--- src/main/java/com/Omarb17/medium_clone/model/dto/request/UserRequestDto.java
+++ src/main/java/com/Omarb17/medium_clone/model/dto/request/UserRequestDto.java
@@ -10,14 +10,16 @@ import lombok.Setter;
 @Setter
 public class UserRequestDto {
 
-    @NotBlank
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
 
-    @NotBlank
-    @Email
+    @Email(message = "Invalid email address")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at leats 8 characters")
     private String password;
 }
